a3ac88c296f6cbb4e44ea7fb11f19611ed8097b9
RahulAid <raccess97@gmail.com>
2022-11-23 01:23:59 +0530

    Add More Elements in the Binary Tree and Check Its Size

--- Program.cs
+++ Program.cs
@@ -9,9 +9,20 @@
             BinarySearchTree<int> objBinary = new BinarySearchTree<int>(56);
             objBinary.Add(30);
             objBinary.Add(70);
-            Console.WriteLine("\nElements in the Binary Tree are : \n");
-            
+            objBinary.Add(22);
+            objBinary.Add(40);
+            objBinary.Add(60);
+            objBinary.Add(95);
+            objBinary.Add(11);
+            objBinary.Add(65);
+            objBinary.Add(3);
+            objBinary.Add(16);
+            objBinary.Add(63);
+            objBinary.Add(67);
+            Console.WriteLine("\nSorted Binary Tree : ");
             objBinary.Display();
+            objBinary.Size();
+            Console.ReadLine();
         }
     }
 }
