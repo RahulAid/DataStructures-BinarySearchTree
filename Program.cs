01e0028b3a4608983156c367c50b1353d78d4553
RahulAid <raccess97@gmail.com>
2022-11-23 01:09:33 +0530

    Create A Binary Search Tree

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructures_BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree Problems \n");
+
+            BinarySearchTree<int> objBinary = new BinarySearchTree<int>(56);
+            objBinary.Add(30);
+            objBinary.Add(70);
+            Console.WriteLine("\nElements in the Binary Tree are : \n");
+            
+            objBinary.Display();
+        }
+    }
+}
